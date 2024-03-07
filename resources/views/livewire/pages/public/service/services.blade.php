38e6aaf25acbd2b7c39f4a0138a59c3d0aa1ef90
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-03-07 17:04:16 +0300

    modified

--- resources/views/livewire/pages/public/service/services.blade.php
+++ resources/views/livewire/pages/public/service/services.blade.php
@@ -6,10 +6,10 @@
     <div
         class="px-4 md:px-72 py-10 bg-gray-300 bg-[url('{{ asset('assets/images/bgs/bg-2.jpg') }}')] bg-cover bg-blend-overlay">
         <div class="">
-            M-SOKO Company Ltd is a well-established supplier specializing in the distribution and sale of ICT
-            (Information and Communication Technology) and construction materials. With over 5 years of experience in
-            the industry, we have been a reliable partner to businesses and construction projects, providing
-            high-quality products and exceptional service
+            At M-SOKO Company Ltd, we specialize in the supply, sale, and maintenance of ICT (Information and
+            Communication Technology) and construction materials. With over 5 years of experience in the industry, we
+            have established ourselves as a trusted provider of high-quality products and reliable services to meet the
+            needs of businesses and construction projects.
         </div>
         <div class="py-8 pb-4">
             <div class="font-bold text-orange-400 hover:text-orange-500">ICT PRODUCTS & SERVICES</div>
@@ -20,14 +20,15 @@
                 <ol>
                     <li class="flex space-x-1 items-center">
                         <div class="">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                             </svg>
                         </div>
                         <div class="">
-                            Hardware: Explore our range of highquality ICT hardware, including desktops, laptops, servers, and networking equipment.
+                            Hardware: Explore our range of highquality ICT hardware, including desktops, laptops,
+                            servers, and networking equipment.
                         </div>
                     </li>
                     <li class="flex space-x-1 items-center">
@@ -39,7 +40,8 @@
                             </svg>
                         </div>
                         <div class="">
-                            Software Solutions: Optimize your operations with customizable software products and solutions designed to boost productivity and safeguard against cyber threats.
+                            Software Solutions: Optimize your operations with customizable software products and
+                            solutions designed to boost productivity and safeguard against cyber threats.
                         </div>
                     </li>
                     <li class="flex space-x-1 items-center">
@@ -51,7 +53,8 @@
                             </svg>
                         </div>
                         <div class="">
-                            Networking & Communication: Stay connected and collaborate effectively with our networking solutions and communication devices.
+                            Networking & Communication: Stay connected and collaborate effectively with our networking
+                            solutions and communication devices.
                         </div>
                     </li>
                     <li class="flex space-x-1 items-center">
@@ -63,12 +66,14 @@
                             </svg>
                         </div>
                         <div class="">
-                            IT Services & Support: Count on our expert team for seamless system installation, maintenance, and reliable technical support.
+                            IT Services & Support: Count on our expert team for seamless system installation,
+                            maintenance, and reliable technical support.
                         </div>
                     </li>
                 </ol>
                 <div class="py-1">
-                    At M-SOKO Company Ltd, we are dedicated to providing innovative ICT products and services to empower your business for success.
+                    At M-SOKO Company Ltd, we are dedicated to providing innovative ICT products and services to empower
+                    your business for success.
                 </div>
             </div>
         </div>
@@ -89,7 +94,8 @@
                             </svg>
                         </div>
                         <div class="">
-                            Cement & Aggregates: Choose from our wide selection of cement products and aggregates to ensure strength and durability in your construction projects.
+                            Cement & Aggregates: Choose from our wide selection of cement products and aggregates to
+                            ensure strength and durability in your construction projects.
                         </div>
                     </li>
                     <li class="flex space-x-1 items-center">
@@ -101,7 +107,8 @@
                             </svg>
                         </div>
                         <div class="">
-                            Steel & Metal Products: Find essential steel and metal products, including beams, columns, and reinforcing bars, to support structural integrity.
+                            Steel & Metal Products: Find essential steel and metal products, including beams, columns,
+                            and reinforcing bars, to support structural integrity.
                         </div>
                     </li>
                     <li class="flex space-x-1 items-center">
@@ -113,7 +120,8 @@
                             </svg>
                         </div>
                         <div class="">
-                            Electrical & Plumbing Supplies: Source all your electrical and plumbing needs from our comprehensive range of supplies for seamless building installations.
+                            Electrical & Plumbing Supplies: Source all your electrical and plumbing needs from our
+                            comprehensive range of supplies for seamless building installations.
                         </div>
                     </li>
                     <li class="flex space-x-1 items-center">
@@ -125,12 +133,14 @@
                             </svg>
                         </div>
                         <div class="">
-                            Tools & Equipment: Access a variety of construction tools and equipment to facilitate safe and efficient project execution, from power tools to safety gear.
+                            Tools & Equipment: Access a variety of construction tools and equipment to facilitate safe
+                            and efficient project execution, from power tools to safety gear.
                         </div>
                     </li>
                 </ol>
                 <div class="py-1">
-                    With a focus on quality, efficiency, and value, M-SOKO Company Ltd is your one-stop destination for premium building and construction materials.
+                    With a focus on quality, efficiency, and value, M-SOKO Company Ltd is your one-stop destination for
+                    premium building and construction materials.
                 </div>
             </div>
         </div>
